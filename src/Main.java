f2f8c47a0a04e63eae4914de03432b1f4e9d8ad6
Dhruv Kotadiya <22ce054@charusat.edu.in>
2023-09-30 08:59:54 +0530

    Added Information of our project in Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("\t\t\tWelcome to the Sales Management System");
+        System.out.println("===========================================================================");
+        System.out.println("\t\t\t    Welcome to our Sales Management System");
+        System.out.println("\n\tOur project facilitates management of product inventory,\n\tpurchasing products, bills report, product report and many more.");
+        System.out.println("\n\tTeam Members:");
+        System.out.println("\t22CE054 Dhruv Kotadiya");
+        System.out.println("\t22CE055 Mehul Kotak");
+        System.out.println("\t22CE056 Parth Kothawala");
+        System.out.println("\t22CE057 Krishna Mali");
+        System.out.println("\t22CE058 Akshay Lakkad");
+        System.out.println("===========================================================================");
         MainMenu.displayMainMenu();
     }
 }
