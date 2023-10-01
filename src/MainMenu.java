72fc5554b86fb90c9c8ef536544acef653f11998
Dhruv Kotadiya <22ce054@charusat.edu.in>
2023-10-01 10:38:40 +0530

    Added 2 more functionalities to main menu

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -3,6 +3,7 @@ public class MainMenu {
     public static void displayMainMenu() {
 
         PublicProduct obj = new PublicProduct();
+        PrivateProduct privateProduct = new PrivateProduct();
         Scanner scanner = new Scanner(System.in);
         int choice;
         do {
@@ -13,6 +14,8 @@ public class MainMenu {
             System.out.println("4. Lifetime Bills/Product Report");
             System.out.println("5. Add Products");
             System.out.println("6. Delete Products");
+            System.out.println("7. Get Last Record in List of Products");
+            System.out.println("8. Get Record Number of Product by Product Code");
             System.out.println("0. Exit");
             System.out.print("Enter your choice: ");
             choice = scanner.nextInt();
@@ -35,6 +38,17 @@ public class MainMenu {
                     break;
                 case 6:
                     obj.deleteProduct();
+                    break;
+                case 7:
+                    System.out.println(privateProduct.getLastRecord(obj.getListOfProducts()));
+                    break;
+                case 8:
+                    int number = privateProduct.getRecordNumberByCode(obj.getListOfProducts());
+                    if(number==-1){
+                        System.out.println("No Product exists with the Entered Code!!!");
+                    }else {
+                        System.out.println("The Record Number: " + number);
+                    }
                     break;
                 case 0:
                     System.out.println("\u001B[32m"+"Thank You for using our Sales Management System."+"\u001B[0m");
